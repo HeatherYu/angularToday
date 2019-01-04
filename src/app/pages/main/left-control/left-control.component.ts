90f1c5515e1f32e5887bf3cf22102114a63a4fed
Heather <1030956860@qq.com>
2019-01-04 18:46:56 +0800

    left

--- src/app/pages/main/left-control/left-control.component.ts
+++ src/app/pages/main/left-control/left-control.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { LocalStorageService } from '../../../services/local-storage.service';
+import { USERNAME } from '../../../services/local-storage.namespace';
 @Component({
   selector: 'app-left-control',
   templateUrl: './left-control.component.html',
   styleUrls: ['./left-control.component.less']
 })
 export class LeftControlComponent implements OnInit {
+  @Input() isCollapsed: boolean;
+  username: string;
 
-  constructor() { }
+  constructor(
+    private store: LocalStorageService
+  ) { }
 
   ngOnInit() {
+    console.log(USERNAME);
+    this.username = this.store.get(USERNAME);
   }
 
 }
